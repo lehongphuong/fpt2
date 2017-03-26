92c4337f9bfecca6f3a7dcac09010510ce102a93
PinkWays <PinkWays@PinkWays-PC.mshome.net>
2017-03-26 23:47:29 +0700

    le hong phuong 26 03

---
+++ src/model/bo/SubmitBO.java
@@ -0,0 +1,51 @@
+package model.bo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import model.bean.Submit;
+import model.dao.SubmitDAO;
+
+public class SubmitBO {
+	SubmitDAO dao = new SubmitDAO();
+
+	/*
+	 * get all categories
+	 */
+	public ArrayList<Submit> getAllSubmit() {
+		return dao.getAllSubmit();
+	}
+	
+	/*
+	 * get all submit leader
+	 */
+	public ArrayList<Submit> getAllSubmitLead(String subId) {
+		return dao.getAllSubmitLead(subId);
+	}
+
+	/*
+	 * get all sumit by user id and subject id
+	 */
+	public ArrayList<Submit> getAllSubmitOfUserWithSubId(String userId,
+			String subId) {
+		return dao.getAllSubmitOfUserWithSubId(userId, subId);
+	}
+
+	public int insertSubmit(Submit submit) {
+		return dao.insertSubmit(submit);
+
+	}
+
+	// update
+	public void updateSubmit(Submit submit) {
+		dao.updateSubmit(submit);
+	}
+
+	// delete
+	public void deleteSubmit(int SubmitId) {
+		dao.deleteSubmit(SubmitId);
+	}
+
+}
